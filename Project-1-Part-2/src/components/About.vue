a558d84c21f1390080791420f12252ff7c483691
Ella Jones <Ella Jones>
2025-11-12 16:16:41 -0500

    about done before styling

--- Project-1-Part-2/src/components/About.vue
+++ Project-1-Part-2/src/components/About.vue
@@ -3,11 +3,22 @@
 
         <h1>Who We Are</h1>
         <h2>Empowered Women. Empowered Finances</h2>
-        <!-- <img src="" alt="WhoWeAreimg"> -->
          <img alt="about-us" src="../assets/about-who-we-are.png" width="125" height="125" />
         <h3>Creating a place for women to become and feel financially self-sufficient. We help set goals, assist needs, and answer questions. Simple. Encouraging. Independent.</h3>
     </div>
 
+    <div>
+        <h1>Our Services</h1>
+         <img alt="about-us" src="../assets/about-our-services.png" width="200" height="125" />
+        <h3>We are able to advise women through understanding what it means to be financially efficient. We also provide a goal planner that helps those step by step through the processes of getting a loan, obtaining a good credit score, saving for a down payment, or even understanding one’s mortgage.</h3>
+        <button>Get Started</button>
+    </div>
+
+    <div>
+        <h1>Our Impact</h1>
+         <img alt="about-us" src="../assets/about-our-impact.png" width="125" height="125" />
+        <h3>We have helped over 50,000 women become financially independent. Over 18,000 of those women were able to pay off their student loans, 16,000 were able to pay off their mortgage, and the other 16,000 were able to gain good credit scores.</h3>
+    </div>
 </template>
 
 <style>
