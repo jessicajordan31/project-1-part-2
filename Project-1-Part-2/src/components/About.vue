d1f0760d5ba36276b56018f2667e4f702a1639c4
Ella Jones <Ella Jones>
2025-11-12 14:43:36 -0500

    imgsAdded

--- Project-1-Part-2/src/components/About.vue
+++ Project-1-Part-2/src/components/About.vue
@@ -1,3 +1,10 @@
 <template>
-    <!-- Comment -->
+    <div id="Who We Are">
+
+        <h1>Who We Are</h1>
+        <h2>Empowered Women. Empowered Finances</h2>
+        <img src="about-who-we-are.png" alt="WhoWeAreimg">
+        <h3>Creating a place for women to become and feel financially self-sufficient. We help set goals, assist needs, and answer questions. Simple. Encouraging. Independent.</h3>
+    </div>
+
 </template>
