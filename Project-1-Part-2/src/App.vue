b84a9015080938bf9fb105c19c6b87a26bcd5bfb
Ella Jones <Ella Jones>
2025-11-12 15:06:37 -0500

    ella starting about

--- Project-1-Part-2/src/App.vue
+++ Project-1-Part-2/src/App.vue
@@ -1,23 +1,25 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import About from './components/About.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- -->
+     <About />
   </main>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
@@ -44,4 +46,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
--- Project-1-Part-2/src/components/About.vue
+++ Project-1-Part-2/src/components/About.vue
@@ -3,8 +3,13 @@
 
         <h1>Who We Are</h1>
         <h2>Empowered Women. Empowered Finances</h2>
-        <img src="about-who-we-are.png" alt="WhoWeAreimg">
+        <!-- <img src="" alt="WhoWeAreimg"> -->
+         <img alt="about-us" src="../assets/about-who-we-are.png" width="125" height="125" />
         <h3>Creating a place for women to become and feel financially self-sufficient. We help set goals, assist needs, and answer questions. Simple. Encouraging. Independent.</h3>
     </div>
 
 </template>
+
+<style>
+    
+</style>
